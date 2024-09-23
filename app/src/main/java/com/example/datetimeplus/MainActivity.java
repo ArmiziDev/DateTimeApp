a6443fd87d66137ebb7b245a92c33b7545e94526
Arman <144297605+ArmiziDev@users.noreply.github.com>
2024-09-23 16:36:36 -0500

    Added Save State / Fixed History
    Now when the app goes into a different state all the elements transfer over to the different xml file.

--- app/src/main/java/com/example/datetimeplus/MainActivity.java
+++ app/src/main/java/com/example/datetimeplus/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.datetimeplus;
 
+import android.content.res.Configuration;
 import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -17,22 +19,71 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText resultValue;
     private TextView outputResultText;
-    private int CurrentTimeUnit = Calendar.MONTH;
+    private LinearLayout resultLayout;
+    private RadioGroup timeUnitRadioGroup;
 
+    private int CurrentTimeUnit = Calendar.MONTH;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        int orientation = getResources().getConfiguration().orientation;
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE)
+        {
+            setContentView(R.layout.activity_main_land);
+        } else {
+            setContentView(R.layout.activity_main);
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        resultValue =findViewById(R.id.AddValueTextInput);
+        resultValue = findViewById(R.id.AddValueTextInput);
         outputResultText = findViewById(R.id.resultTextOutput);
+        resultLayout = findViewById(R.id.ResultScrollViewLinearLayout);
+        timeUnitRadioGroup = findViewById(R.id.TimeUnitRadioGroup);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState)
+    {
+        super.onSaveInstanceState(savedInstanceState);
+
+        savedInstanceState.putString("outputResultText", outputResultText.getText().toString());
+        savedInstanceState.putString("resultValue", resultValue.getText().toString());
+        savedInstanceState.putInt("timeUnitRadioGroup", timeUnitRadioGroup.getCheckedRadioButtonId());
+        int historyTextCount = resultLayout.getChildCount();
+        String[] resultHistory = new String[historyTextCount];
+        for (int i = 0; i < historyTextCount; i++)
+        {
+            TextView child = (TextView) resultLayout.getChildAt(i);
+            resultHistory[i] = child.getText().toString();
+        }
+        savedInstanceState.putStringArray("resultHistory", resultHistory);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState)
+    {
+        outputResultText.setText(savedInstanceState.getString("outputResultText"));
+        resultValue.setText(savedInstanceState.getString("resultValue"));
+        timeUnitRadioGroup.check(savedInstanceState.getInt("timeUnitRadioGroup"));
+        String[] resultHistory = savedInstanceState.getStringArray("resultHistory");
+        if (resultHistory != null)
+            for (String history : resultHistory)
+            {
+                addHistoryText(history);
+            }
+    }
+
+    private void addHistoryText(String history) {
+        TextView newResultHistoryText = new TextView(this);
+        newResultHistoryText.setTextSize(18);
+        newResultHistoryText.setText(history);
+        resultLayout.addView(newResultHistoryText);
     }
 
     public void TimeUnitRadioSelection(View v)
@@ -47,6 +98,22 @@ public class MainActivity extends AppCompatActivity {
         } else if (id == R.id.MonthsRadioButton) {
             CurrentTimeUnit = Calendar.MONTH;
         }
+    }
+
+    private String getTimeUnit()
+    {
+        switch (CurrentTimeUnit)
+        {
+            case Calendar.HOUR:
+                return "Hour";
+            case Calendar.DAY_OF_YEAR:
+                return "Day";
+            case Calendar.WEEK_OF_YEAR:
+                return "Week";
+            case Calendar.MONTH:
+                return "Month";
+        }
+        return "";
     }
 
     public void Calculate(View v)
@@ -64,10 +131,12 @@ public class MainActivity extends AppCompatActivity {
 
         outputResultText.setText(cal.getTime().toString());
 
-        LinearLayout resultLayout = findViewById(R.id.ResultScrollViewLinearLayout);
-        TextView newResultHistoryText = new TextView(this);
+        String newString = "+" + value_string + " " + getTimeUnit() + ": " + cal.getTime().toString();
+        addHistoryText(newString);
+    }
 
-        newResultHistoryText.setText("+" + value_string + "" + cal.getTime().toString());
-        resultLayout.addView(newResultHistoryText);
+    public void ClearButton(View v)
+    {
+        resultLayout.removeAllViews();
     }
 }
